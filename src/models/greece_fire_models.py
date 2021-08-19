528c17529f4cd1e1bb46d471229f18d62e876bb7
iprapas <22853667+iprapas@users.noreply.github.com>
2021-08-19 11:33:20 +0300

    Add convLSTM model

--- src/models/greece_fire_models.py
+++ src/models/greece_fire_models.py
@@ -33,6 +33,175 @@ import random
 from tqdm import tqdm
 from joblib import Parallel, delayed
 from sklearn.preprocessing import StandardScaler
+from src.models.modules.convlstm import ConvLSTM
+
+
+class ConvLSTM_fire_model(LightningModule):
+    """
+    Example of LightningModule for MNIST classification.
+
+    A LightningModule organizes your PyTorch code into 5 sections:
+        - Computations (init).
+        - Train loop (training_step)
+        - Validation loop (validation_step)
+        - Test loop (test_step)
+        - Optimizers (configure_optimizers)
+
+    Read the docs:
+        https://pytorch-lightning.readthedocs.io/en/latest/common/lightning_module.html
+    """
+
+    def __init__(
+            self,
+            dynamic_features=None,
+            static_features=None,
+            hidden_size: int = 32,
+            lstm_layers: int = 3,
+            lr: float = 0.001,
+            positive_weight: float = 0.8,
+            lr_scheduler_step: int = 10,
+            lr_scheduler_gamma: float = 0.1,
+            weight_decay: float = 0.0005):
+        super().__init__()
+        # this line ensures params passed to LightningModule will be saved to ckpt
+        # it also allows to access params with 'self.hparams' attribute
+        if static_features is None:
+            static_features = []
+        if dynamic_features is None:
+            dynamic_features = []
+        self.save_hyperparameters()
+
+        # clstm part
+        self.convlstm = ConvLSTM(len(dynamic_features) + len(static_features),
+                                 hidden_size,
+                                 (3, 3),
+                                 lstm_layers,
+                                 True,
+                                 True,
+                                 False)
+        # cnn part
+        self.conv1 = nn.Conv2d(hidden_size, 8, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(8, 16, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(16, 16, kernel_size=3, stride=1, padding=1)
+        # fully-connected part
+        self.fc1 = nn.Linear(576, 64)
+        self.drop1 = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(64, 32)
+        self.drop2 = nn.Dropout(0.5)
+        self.fc3 = nn.Linear(32, 2)
+
+        # loss function
+        self.criterion = torch.nn.NLLLoss(weight=torch.tensor([1 - positive_weight, positive_weight]))
+        # use separate metric instance for train, val and test step
+        # to ensure a proper reduction over the epoch
+
+        # Accuracy, AUROC, AUC, ConfusionMatrix
+        self.train_accuracy = Accuracy()
+        self.train_auc = AUROC(num_classes=2, pos_label=1)
+
+        self.val_accuracy = Accuracy()
+        self.val_auc = AUROC(num_classes=2, pos_label=1)
+
+        self.test_accuracy = Accuracy()
+        self.test_auc = AUROC(num_classes=2, pos_label=1)
+
+        self.lr_scheduler_step = lr_scheduler_step
+        self.lr_scheduler_gamma = lr_scheduler_gamma
+        self.lr = lr
+        self.weight_decay = weight_decay
+
+    def forward(self, x: torch.Tensor):
+        _, last_states = self.convlstm(x)
+        x = last_states[0][0]
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = F.max_pool2d(F.relu(self.conv3(x)), 2)
+        x = torch.flatten(x, 1)
+        x = F.relu(self.drop1(self.fc1(x)))
+        x = F.relu(self.drop2(self.fc2(x)))
+        return torch.nn.functional.log_softmax(self.fc3(x), dim=1)
+
+    def step(self, batch: Any):
+        dynamic, static, _, y = batch
+        y = y.long()
+        bsize, timesteps, _, _, _ = dynamic.shape
+        static = static.unsqueeze(dim=1)
+        repeat_list = [1 for _ in range(static.dim())]
+        repeat_list[1] = timesteps
+        static = static.repeat(repeat_list)
+
+        # print(dynamic.shape, static.shape)
+        inputs = torch.cat([dynamic, static], dim=2).float()
+        logits = self.forward(inputs)
+        loss = self.criterion(logits, y)
+        preds = torch.argmax(logits, dim=1)
+        preds_proba = torch.exp(logits)[:, 1]
+        return loss, preds, preds_proba, y
+
+    def training_step(self, batch: Any, batch_idx: int):
+        loss, preds, preds_proba, targets = self.step(batch)
+
+        # log train metrics
+        acc = self.train_accuracy(preds, targets)
+        auc = self.train_auc(preds_proba, targets)
+
+        self.log("train/loss", loss, on_step=False, on_epoch=True, prog_bar=False)
+        self.log("train/acc", acc, on_step=False, on_epoch=True, prog_bar=True)
+        self.log("train/auc", auc, on_step=False, on_epoch=True, prog_bar=True)
+        # we can return here dict with any tensors
+        # and then read it in some callback or in training_epoch_end() below
+        # remember to always return loss from training_step, or else backpropagation will fail!
+        return {"loss": loss, "preds": preds, "targets": targets}
+
+    def training_epoch_end(self, outputs: List[Any]):
+        # `outputs` is a list of dicts returned from `training_step()`
+        pass
+
+    def validation_step(self, batch: Any, batch_idx: int):
+        loss, preds, preds_proba, targets = self.step(batch)
+
+        # log val metrics
+        acc = self.val_accuracy(preds, targets)
+        auc = self.val_auc(preds_proba, targets)
+
+        self.log("val/loss", loss, on_step=False, on_epoch=True, prog_bar=False)
+        self.log("val/acc", acc, on_step=False, on_epoch=True, prog_bar=True)
+        self.log("val/auc", auc, on_step=False, on_epoch=True, prog_bar=True)
+
+        return {"loss": loss, "preds": preds, "targets": targets}
+
+    def validation_epoch_end(self, outputs: List[Any]):
+        pass
+
+    def test_step(self, batch: Any, batch_idx: int):
+        loss, preds, preds_proba, targets = self.step(batch)
+
+        # log test metrics
+        acc = self.test_accuracy(preds, targets)
+        auc = self.test_auc(preds_proba, targets)
+
+        self.log("test/loss", loss, on_step=False, on_epoch=True)
+        self.log("test/acc", acc, on_step=False, on_epoch=True)
+        self.log("test/auc", auc, on_step=False, on_epoch=True)
+
+        return {"loss": loss, "preds": preds, "targets": targets}
+
+    def test_epoch_end(self, outputs: List[Any]):
+        pass
+
+    def configure_optimizers(self):
+        """Choose what optimizers and learning-rate schedulers to use in your optimization.
+        Normally you'd need one. But in the case of GANs or similar you might have multiple.
+
+        See examples here:
+            https://pytorch-lightning.readthedocs.io/en/latest/common/lightning_module.html#configure-optimizers
+        """
+        optimizer = torch.optim.Adam(
+            params=self.parameters(), lr=self.hparams.lr, weight_decay=self.hparams.weight_decay
+        )
+        lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=self.lr_scheduler_step,
+                                                       gamma=self.lr_scheduler_gamma)
+        return {'optimizer': optimizer, 'lr_scheduler': lr_scheduler}
 
 
 class LSTM_fire_model(LightningModule):
@@ -240,8 +409,11 @@ class CNN_fire_model(LightningModule):
         self.conv2 = nn.Conv2d(8, 16, kernel_size=3, stride=1, padding=1)
         self.conv3 = nn.Conv2d(16, 16, kernel_size=3, stride=1, padding=1)
         self.fc1 = nn.Linear(576, 64)
-        self.fc2 = nn.Linear(64, 2)
-        self.drop = nn.Dropout(0.5)
+        self.drop1 = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(64, 32)
+        self.drop2 = nn.Dropout(0.5)
+        self.fc3 = nn.Linear(32, 2)
+
         assert (positive_weight < 1) and (positive_weight > 0)
         self.positive_weight = positive_weight
 
@@ -270,9 +442,9 @@ class CNN_fire_model(LightningModule):
         x = F.max_pool2d(F.relu(self.conv2(x)), 2)
         x = F.max_pool2d(F.relu(self.conv3(x)), 2)
         x = torch.flatten(x, 1)
-        x = F.relu(self.drop(self.fc1(x)))
-        x = F.relu(self.fc2(x))
-        return torch.nn.functional.log_softmax(x, dim=1)
+        x = F.relu(self.drop1(self.fc1(x)))
+        x = F.relu(self.drop2(self.fc2(x)))
+        return torch.nn.functional.log_softmax(self.fc3(x), dim=1)
 
     def step(self, batch: Any):
         dynamic, static, clc, y = batch
