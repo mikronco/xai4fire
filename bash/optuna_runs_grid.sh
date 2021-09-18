e97a9a0c9d07d7bac747f9e080eb638da2d011fa
iprapas <22853667+iprapas@users.noreply.github.com>
2021-09-18 09:19:58 +0300

    Refactor grid param search bash script

--- bash/optuna_runs_grid.sh
+++ bash/optuna_runs_grid.sh
@@ -4,10 +4,10 @@
 export CUDA_VISIBLE_DEVICES=1
 
 echo "Running experiment optuna lstm_temporal_cls"
-python run.py -m hparams_search=greecefire_optuna experiment=lstm_temporal_cls >> optuna_runs_lstm.log &
+python run.py -m hparams_search=greecefire_optuna_grid experiment=lstm_temporal_cls >> optuna_runs_lstm.log &
 
 echo "Running experiment optuna cnn_spatial_cls"
-python run.py -m hparams_search=greecefire_optuna experiment=cnn_spatial_cls >> optuna_runs_cnn.log
+python run.py -m hparams_search=greecefire_optuna_grid experiment=cnn_spatial_cls >> optuna_runs_cnn.log
 
 echo "Running experiment optuna clstm_spatiotemporal_cls"
-python run.py -m hparams_search=greecefire_optuna experiment=clstm_spatiotemporal_cls
+python run.py -m hparams_search=greecefire_optuna_grid experiment=clstm_spatiotemporal_cls >> optuna_runs_cnn.log
