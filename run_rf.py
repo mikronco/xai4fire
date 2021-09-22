ac403b6e162caf820e5f25faf52bd73f548fe7cb
iprapas <22853667+iprapas@users.noreply.github.com>
2021-09-22 22:56:19 +0300

    Add permutation importance for RF

--- run_rf.py
+++ run_rf.py
@@ -8,25 +8,37 @@ from sklearn.metrics import confusion_matrix, plot_confusion_matrix, classificat
 from matplotlib import pyplot as plt
 
 features_ = [
-    # 'Fpar_500m',
-    # 'Lai_500m',
+    'Fpar_500m',
+    'Lai_500m',
     'LST_Day_1km',
     'LST_Night_1km',
     '1 km 16 days NDVI',
-    # '1 km 16 days EVI',
-    'ET_500m',
+    '1 km 16 days EVI',
+    # 'ET_500m',
     # 'LE_500m',
     # 'PET_500m',
     # 'PLE_500m',
-    # 'era5_max_u10',
-    # 'era5_max_v10',
-    # 'era5_max_t2m',
-    # 'era5_max_tp',
-    # 'era5_min_u10',
-    # 'era5_min_v10',
-    # 'era5_min_t2m',
+    'era5_max_u10',
+    'era5_max_v10',
+    'era5_max_t2m',
+    'era5_max_tp',
+    'era5_min_u10',
+    'era5_min_v10',
+    'era5_min_t2m',
     # 'era5_min_tp',
-    'fwi',
+    # 'fwi',
+]
+
+dynamic_features = [
+    'LST_Day_1km',
+    'LST_Night_1km',
+    'era5_max_u10',
+    'era5_max_v10',
+    'era5_max_t2m',
+    'era5_max_tp',
+    'era5_min_u10',
+    'era5_min_v10',
+    'era5_min_t2m',
 ]
 
 features = features_ + [f'{x}_10day_mean' for x in features_]
@@ -34,11 +46,11 @@ features = features_ + [f'{x}_10day_mean' for x in features_]
 coordinates = ['x', 'y']
 
 static_features = [
-    # 'dem_mean',
+    'dem_mean',
     # 'dem_std',
     'aspect_mean',
     # 'aspect_std',
-    # 'slope_mean',
+    'slope_mean',
     # 'slope_std',
     'roads_density_2020',
     'population_density',
@@ -72,9 +84,15 @@ dataset_path = Path.home() / 'jh-shared/iprapas/uc3/datasets/pixel_dataset_clc_w
 
 if __name__ == '__main__':
     ds = xr.open_dataset(dataset_path)
+    dates = pd.DatetimeIndex(ds['time_'].values)
 
-    train_ds = ds.isel(patch=slice(0, 17068))
-    test_ds = ds.isel(patch=slice(17068, len(ds.patch)))
+    val_year = 2020
+    test_year = 2020
+    train_ds = ds.isel(patch=list(np.where(~dates.year.isin([val_year, test_year]))[0]))
+    val_ds = ds.isel(patch=list(np.where(dates.year == val_year)[0]))
+    test_ds = ds.isel(patch=list(np.where(dates.year == test_year)[0]))
+    # train_ds = ds.isel(patch=slice(0, 17068))
+    # test_ds = ds.isel(patch=slice(17068, len(ds.patch)))
 
     train_dict = {}
     test_dict = {}
@@ -83,7 +101,7 @@ if __name__ == '__main__':
         train_dict[feat] = train_ds[feat].values
         test_dict[feat] = test_ds[feat].values
 
-    nan_value = -999
+    nan_value = -9999
     train_df = pd.DataFrame.from_dict(train_dict).fillna(nan_value)
     test_df = pd.DataFrame.from_dict(test_dict).fillna(nan_value)
 
@@ -91,7 +109,7 @@ if __name__ == '__main__':
     X_train, X_test = train_df[features_filtered + static_features], test_df[features_filtered + static_features]
     y_train, y_test = train_df[target], test_df[target]
 
-    clf = RandomForestClassifier(n_estimators=500, criterion='gini')
+    clf = RandomForestClassifier(n_estimators=500)
     clf.fit(X_train, y_train)
     y_pred = clf.predict(X_test)
 
@@ -107,3 +125,20 @@ if __name__ == '__main__':
     precision, recall, _ = precision_recall_curve(y_test, y_pred_proba)
     print("AUPRC:", auc(recall, precision))
     print("AUROC:", roc_auc_score(y_test, y_pred_proba))
+
+    from sklearn.inspection import permutation_importance
+    import time
+    start_time = time.time()
+    result = permutation_importance(
+        clf, X_test, y_test, n_repeats=10, random_state=42, n_jobs=2)
+    elapsed_time = time.time() - start_time
+    print(f"Elapsed time to compute the importances: "
+          f"{elapsed_time:.3f} seconds")
+
+    forest_importances = pd.Series(result.importances_mean, index=features+static_features)
+    fig, ax = plt.subplots()
+    forest_importances.plot.bar(yerr=result.importances_std, ax=ax)
+    ax.set_title("Feature importances using permutation on full model")
+    ax.set_ylabel("Mean accuracy decrease")
+    fig.tight_layout()
+    plt.show()
